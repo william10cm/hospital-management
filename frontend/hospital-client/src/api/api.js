d24664dac0e7150540a5cd9f294fd59cf216cba5
william10cm <205263622+william10cm@users.noreply.github.com>
2026-05-06 15:28:29 -0400

    api updates

--- frontend/hospital-client/src/api/api.js
+++ frontend/hospital-client/src/api/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://hospital-management-xzxg.onrender.com/api";
 
 export async function loginUser(email, password) {
   const response = await fetch(`${API_BASE_URL}/auth/login`, {
